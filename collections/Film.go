d8f1fb9489968ede3ecf4b820c25047b704a396c
Abubakar Sadiq Hassan <abubakarhassan59@gmail.com>
2021-06-07 22:46:24 +0100

    Modified film collection

--- collections/Film.go
+++ collections/Film.go
@@ -1,5 +1,9 @@
 package collections
 
+type FilmCollection struct {
+	Results []Film `json:"results"`
+}
+
 type Film struct {
 	Title        string `json:"title"`
 	OpeningCrawl string `json:"opening_crawl"`
